1b0542bfef67ae6e821818b5349f193cc3c84404
Bai-xiaoyan <1486611617@qq.com>
2019-02-21 19:29:48 +0800

    Create get_vector.py

---
+++ get_vector.py
@@ -0,0 +1,30 @@
+ #!/usr/bin/env python                                                                                                                                                                  
+ # -*- coding: utf-8 -*-
+ # encoding: utf-8
+ 
+ #逐行读取文件数据进行jieba分词
+ 
+ import jieba
+ import jieba.analyse
+ import jieba.posseg as pseg  #引入词性标注接口
+ import codecs,sys
+ 
+ 
+ f = codecs.open('data.txt' , 'r', encoding = 'utf8')
+ target = codecs.open('data_seg.txt', 'w', encoding = 'utf8')
+ stopkey = [w.strip() for w in codecs.open('stopWrod.txt', 'r').readlines()]
+ print("open files")
+ #count = 0
+ lineNum = 1
+ line = f.readline()
+ while line:
+     print("---processiong", lineNum, "article---")
+     seg_list = jieba.cut(line, cut_all = False)
+     line_seg = ' '.join(seg_list)
+     target.writelines(line_seg)
+     lineNum += 1
+     line = f.readline()
+ 
+ print("well done")
+ f.close()
+ target.close()
